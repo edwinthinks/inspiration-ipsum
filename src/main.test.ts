afeb4011c0031ad4c23224a4ebaa9a3e1a9dc0df
Edwin Mak <edwinthinks@gmail.com>
2019-11-11 06:59:30 -0500

    Add basic testing using mocha without express specific testing

---
+++ src/main.test.ts
@@ -0,0 +1,9 @@
+import app from './main';
+import { expect } from 'chai';
+
+describe('addition', () => {
+  it('returns 5 when adding 2 and 3', () => {
+    expect(2+3).to.equal(5);
+  });
+
+});
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,5 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+export default app;
