9235f9628f372dc67bb0f32cf43a7483b3fd5444
Edwin Mak <edwinthinks@gmail.com>
2019-11-12 18:22:17 -0500

    Add a procfile and the port reading

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./src/build/app.js
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 
 if (process.env.NODE_ENV !== "production") {
+  // Read from the .env file if not on production.
   require("dotenv").config();
 }
 
-// Create a new express application instance
 const app: express.Application = express();
+const port: number = Number(process.env.PORT) || 3000;
 
 app.get("/", function(req, res) {
   res.send({ hello: "world" });
 });
 
-app.listen(3000, function() {
-  console.log("Starting app on port 3000!");
+app.listen(port, function() {
+  console.log(`Starting app on port ${port}`);
 });
 
 export { app };
