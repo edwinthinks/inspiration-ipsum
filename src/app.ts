f9b8794bcb5323ea4890956941d5e4c99f362f38
Edwin Mak <edwinthinks@gmail.com>
2019-11-13 08:28:53 -0500

    Make deployable to heroku

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import path from "path";
 
 if (process.env.NODE_ENV !== "production") {
   // Read from the .env file if not on production.
   require("dotenv").config();
 }
+
 const app: express.Application = express();
 const port: number = Number(process.env.PORT) || 3001;
+
+app.use(express.static(path.join(__dirname, "/../client/build")));
 
 app.get("/users", function(req, res) {
   res.json([
@@ -18,6 +22,10 @@ app.get("/users", function(req, res) {
       username: "D0loresH4ze"
     }
   ]);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 app.listen(port, function() {
