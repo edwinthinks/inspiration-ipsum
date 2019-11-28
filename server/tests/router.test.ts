51660963aefaf90ea4d5438f1e01f67c556bb4dc
Edwin Mak <edwinthinks@gmail.com>
2019-11-28 12:20:33 -0500

    Enable debugging (#40)
    * Add script command to enable debugging
    
    * Update with coverage checking
    
    * Add test coverage thresholds
    
    * ADd test for router and avoid async
    
    * Update

---
+++ server/tests/router.test.ts
@@ -0,0 +1,38 @@
+import sinon, { SinonSandbox } from "sinon";
+import request from "supertest";
+
+import Server from "../src/server";
+
+describe("Router", () => {
+  let sinonSandbox: SinonSandbox;
+  let server: any;
+
+  beforeEach(() => {
+    sinonSandbox = sinon.createSandbox();
+    server = new Server().start();
+  });
+
+  afterEach(() => {
+    sinonSandbox.restore();
+    server.close();
+  });
+
+  describe("match non existent route", () => {
+    let fakeQuotes = [
+      { quote: "Fake", author: "Fake Author" },
+      { quote: "Fake 2", author: "Fake Author 2" }
+    ];
+
+    beforeEach(() => {
+      // Stub the output of Quotes to provide a specific
+      // and expected output
+      // sinonSandbox.stub(Quotes, "getAll").returns(fakeQuotes);
+    });
+
+    it("returns 200 and a collection of quotes", async () => {
+      await request(server)
+        .get("/unknown-path")
+        .expect("Content-Type", /html/)
+    });
+  });
+});
