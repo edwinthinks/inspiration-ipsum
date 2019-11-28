12f6a16227884fb5923679e2711ebf6087d4afc1
Edwin Mak <edwinthinks@gmail.com>
2019-11-28 11:34:32 -0500

    Add test coverage thresholds

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: [
-    './tests/setup-tests.js'
-  ]
+  setupFilesAfterEnv: ['./tests/setup-tests.js'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 35,
+      lines: 80,
+      functions: 0,
+    },
+  },
 };
